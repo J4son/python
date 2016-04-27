ad1f01a3d1f602ffbfa0553ab46823c5f7ee2b2f
daytona675 <secuinfo28@gmail.com>
2016-04-27 22:06:27 +0200

    file creation

--- rootme_irc.py
+++ rootme_irc.py
@@ -7,15 +7,17 @@ my_nickname = "daytona675"
 channels = "#root-me_challenge"
 
 c =irc.client.ServerConnection(irc.client.Reactor())
-c.buffer_class = irc.buffer.LineBuffer
+c.buffer_class = client.buffer.LineBuffer
 c.buffer_class.errors = 'replace'
+
 try:
     b = c.reactor.server().connect("irc.root-me.org",6667,my_nickname,channels)
-except irc.client.ServerConnectionError as x:
-        print("erreur:",x)
+except irc.client.ServerConnectionError as erreur:
+        print("erreur:",erreur)
         sys.exit(1)
+
 b.join("#root-me_challenge")
 b.is_connected()
 b.privmsg("daytona675","hello")
-b.socket.recvmsg(2048)
+
 
